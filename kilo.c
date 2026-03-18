3a8f76b2a8e5c93173cb4a56859695820fd752c4
Dmytro Chumakov <dmytro.v.chumakov.work@gmail.com>
2026-03-18 16:24:36 +0200

    clear the screen

--- kilo.c
+++ kilo.c
@@ -59,6 +59,9 @@ char editorReadKey() {
   return c;
 }
 
+/*** output ***/
+void editorRefreshScreen() { write(STDOUT_FILENO, "\x1b[2J", 4); }
+
 /*** input ***/
 
 void editorProcessKeypress() {
@@ -77,6 +80,7 @@ int main() {
   enableRawMode();
 
   while (1) {
+    editorRefreshScreen();
     editorProcessKeypress();
   }
 
