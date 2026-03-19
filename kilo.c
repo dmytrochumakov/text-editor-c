ba6435f9bdffc0e3c2ee1f7818a2b1073a9f389a
Dmytro Chumakov <dmytro.v.chumakov.work@gmail.com>
2026-03-19 16:50:58 +0200

    clear the screen on exit

--- kilo.c
+++ kilo.c
@@ -18,6 +18,9 @@ struct termios orig_termios;
 /*** terminal ***/
 
 void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
   perror(s);
   exit(1);
 }
@@ -60,9 +63,9 @@ char editorReadKey() {
 }
 
 /*** output ***/
-void editorRefreshScreen() { 
-	write(STDOUT_FILENO, "\x1b[2J", 4); 
-	write(STDOUT_FILENO, "\x1b[H", 3);
+void editorRefreshScreen() {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
 /*** input ***/
@@ -72,6 +75,9 @@ void editorProcessKeypress() {
 
   switch (c) {
   case CTRL_KEY('q'):
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
     exit(0);
     break;
   }
