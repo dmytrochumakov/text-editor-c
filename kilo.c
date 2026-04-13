98d9fb5f8215c04e07e0946d74675b4617170be6
Dmytro Chumakov <dmytro.v.chumakov.work@gmail.com>
2026-04-13 17:07:30 +0300

    vertical scrolling

--- kilo.c
+++ kilo.c
@@ -38,6 +38,7 @@ typedef struct erow {
 
 struct editorConfig {
   int cx, cy;
+  int row_offset;
   int screenrows;
   int screencols;
   int numrows;
@@ -263,10 +264,21 @@ void abAppend(struct abuf *ab, const char *s, int len) {
 void abFree(struct abuf *ab) { free(ab->b); }
 
 /*** output ***/
+
+void editorScroll() {
+  if (E.cy < E.row_offset) {
+    E.row_offset = E.cy;
+  }
+  if (E.cy > E.row_offset) {
+    E.row_offset = E.cy - E.screenrows + 1;
+  }
+}
+
 void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    if (y >= E.numrows) {
+    int filerow = y + E.row_offset;
+    if (filerow >= E.numrows) {
       if (E.numrows == 0 && y == E.screenrows / 3) {
         char welcome[80];
         int welcomelen = snprintf(welcome, sizeof(welcome),
@@ -290,11 +302,11 @@ void editorDrawRows(struct abuf *ab) {
         abAppend(ab, "~", 1);
       }
     } else {
-      int len = E.row[y].size;
+      int len = E.row[filerow].size;
       if (len > E.screencols) {
         len = E.screencols;
       }
-      abAppend(ab, E.row[y].chars, len);
+      abAppend(ab, E.row[filerow].chars, len);
     }
 
     abAppend(ab, "\x1b[K", 3);
@@ -306,6 +318,8 @@ void editorDrawRows(struct abuf *ab) {
 }
 
 void editorRefreshScreen() {
+  editorScroll();
+
   struct abuf ab = ABUF_INIT;
 
   abAppend(&ab, "\x1b[?25l", 6);
@@ -314,7 +328,7 @@ void editorRefreshScreen() {
   editorDrawRows(&ab);
 
   char buf[32];
-  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.row_offset) + 1, E.cx + 1);
   abAppend(&ab, buf, strlen(buf));
 
   abAppend(&ab, "\x1b[?25h", 6);
@@ -327,7 +341,7 @@ void editorRefreshScreen() {
 void editorMoveCursor(int key) {
   switch (key) {
   case ARROW_DOWN:
-    if (E.cy != E.screenrows - 1) {
+    if (E.cy < E.numrows) {
       E.cy++;
     }
     break;
@@ -389,6 +403,7 @@ void editorProcessKeypress() {
 void initEditor() {
   E.cx = 0;
   E.cy = 0;
+  E.row_offset = 0;
   E.numrows = 0;
   E.row = NULL;
 
